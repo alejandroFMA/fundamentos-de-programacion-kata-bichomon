f4026984bf7fe22855658ee0b59279b50cf5c210
alejandroFMA <alexfmarquez@gmail.com>
2023-10-02 20:55:29 +0200

    update

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 console.log(document.title);
+document.querySelector("#gen-1").innerHTML="Generasio Pokimon 1";
+//  
+var backPoke = document.querySelectorAll(".infocard ")
+for (let i = 0; i < backPoke.length; i++){
+  backPoke[i] = backPoke[i].style.background="red";
+}
+//
+ console.log(document.URL);
+// 
+console.log(document.domain);
+// 
+let nodoImg = document.querySelectorAll("img");
+console.log(nodoImg)
+// 
+var changeImg = document.querySelectorAll("img");
+
+ for (let i = 0; i < changeImg.length; i++){
+
+changeImg[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+
+}
